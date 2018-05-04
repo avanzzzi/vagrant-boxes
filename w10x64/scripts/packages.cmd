3a774377487d183887a641a9cd2597775435ac8a
Gabriel Avanzi <gabriel.avanzi@gmail.com>
2018-05-04 11:12:16 -0300

    Include basic provisiong for windows using chocolatey

---
+++ w10x64/scripts/packages.cmd
@@ -0,0 +1,12 @@
+chocolatey feature enable -n=allowGlobalConfirmation
+
+choco install vim
+choco install git
+choco install python
+choco install googlechrome
+choco install firefox
+
+chocolatey feature disable -n=allowGlobalConfirmation
+
+refreshenv
+pip install ipython
