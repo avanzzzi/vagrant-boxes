1cadd49caca16d36a0b832df01fc962ceec8e2bb
Gabriel Avanzi <gabriel.avanzi@gmail.com>
2018-05-17 08:57:34 -0300

    Remove disable_ipv6 prompt when reg key already exists

--- w10x64/scripts/disable_ipv6.cmd
+++ w10x64/scripts/disable_ipv6.cmd
@@ -1,1 +1,1 @@
-reg add hklm\system\currentcontrolset\services\tcpip6\parameters /v DisabledComponents /t REG_DWORD /d 0xFFFFFFFF
+reg add hklm\system\currentcontrolset\services\tcpip6\parameters /v DisabledComponents /t REG_DWORD /d 0xFFFFFFFF /f
